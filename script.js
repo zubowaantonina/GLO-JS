1dc410144deb085a9b3f66aad108cd4136d5fc03
zubowaantonina <zubowaantonina@yandex.ru>
2024-03-28 15:35:38 +0300

    lesson01

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello, GLO');
+let title = 'H1';
+let screens = 'screens';
+let screenPrice = '10';
+let rollback = 'true'; 
+let fullPrice = '100';
+let adaptive='false';
+alert("Enter price" + fullPrice);
+console.log(rollback);
