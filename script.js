20801efe9d4ff60c03537207d627271ef1ef1ab5
zubowaantonina <zubowaantonina@yandex.ru>
2024-03-29 16:57:39 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
-let title = 'H1';
-let screens = 'screens';
-let screenPrice = '10';
-let rollback = 'true'; 
-let fullPrice = '100';
-let adaptive='false';
-alert("Enter price" + fullPrice);
-console.log(rollback);
+let title = 'Курс "JavaScript"';
+console.log(typeof title);
+let screens = 'Простые, Сложные, Интерактивные';
+console.log(screens.length);
+let screenPrice = 10;
+console.log('Стоимость верстки экранов  ' + screenPrice + ' рублей/ долларов/гривен/юани');
+let rollback = 10;
+let fullPrice = 50000;
+console.log('“Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+console.log(typeof fullPrice);
+let adaptive = false;
+console.log(typeof adaptive);
+
+
+screens = screens.toLowerCase();//приводим к нижнему регистру
+screens = screens.split(/\s*,\s*/);//разбиваем строку на массив
+console.log(screens);
+
+
+console.log( 'Процент отката посреднику за работу ' + fullPrice * (rollback/100)+' рублей');//Процент отката посреднику за работу
